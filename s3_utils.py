cc97885695ee2ae5a83b31f13ff0e7ad2ece293c
paul_m <paulomerieux@gmail.com>
2026-07-03 16:28:06 +0200

    modification run_all, et s3_utils

--- s3_utils.py
+++ s3_utils.py
@@ -4,38 +4,33 @@ import io
 import pandas as pd
 from dotenv import load_dotenv
 
-# 1. INITIALISATION (Le portier)
 load_dotenv()
 
-def get_s3_client():
-    """Étape 0 : Allumer la télécommande AWS"""
-    return boto3.client('s3')
 
-# 2. LECTURE (Le drone va chercher le linge sale dans Bronze)
 def read_s3_csv(file_key, separator=';'):
-    """Étape 1 : Lire un fichier depuis S3 et le mettre dans un tableau Python"""
-    s3 = get_s3_client()
+
+    """Lecture d'un fichier depuis S3 et stockage dans un tableau Python"""
+
+    s3 = boto3.client('s3')
     bucket = "projet-accidents-jedha"
-    
-    # On demande l'objet à AWS
     response = s3.get_object(Bucket=bucket, Key=file_key)
-    # On transforme les octets reçus en un tableau (DataFrame) que tu peux lire
-    df = pd.read_csv(io.BytesIO(response['Body'].read()), sep=separator)
+
+    df = pd.read_csv(io.BytesIO(response['Body'].read()), sep=separator) #conversion en DataFrame
+
     return df
 
-# 3. ÉCRITURE (Le drone range le linge propre dans Silver)
+
 def upload_to_s3(df, file_name, folder="silver"):
+
     """Étape 2 : Envoyer le tableau nettoyé vers S3"""
-    s3 = get_s3_client()
+
+    s3 = boto3.client('s3')
     bucket = "projet-accidents-jedha"
-    
-    # On transforme le tableau en texte (CSV) dans la mémoire vive
+
     csv_buffer = io.StringIO()
     df.to_csv(csv_buffer, index=False)
     
-    # On définit l'adresse d'arrivée
     target_key = f"{folder}/{file_name}"
     
-    # On envoie le paquet
     s3.put_object(Bucket=bucket, Key=target_key, Body=csv_buffer.getvalue())
-    print(f"✅ Mission accomplie : {target_key} est sur S3 !")
+    print(f"{target_key} est sur S3 !")
--- script_connexion_RDS.py
+++ script_connexion_RDS.py
@@ -6,23 +6,18 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 from dotenv import load_dotenv
 
-# 1. Chargement des variables d'environnement
+#Chargement des variables d'environnement
 load_dotenv()
-
-# Configuration AWS (S3)
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_DEFAULT_REGION", "eu-west-3")
 BUCKET_NAME = "projet-accidents-jedha"
-
-# Configuration Base de données (RDS)
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME")
 
-# 2. Initialisation des clients
 s3_client = boto3.client(
     's3',
     aws_access_key_id=AWS_ACCESS_KEY,
@@ -33,7 +28,7 @@ s3_client = boto3.client(
 connection_url = f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(connection_url)
 
-# 3. CONFIGURATION DES TABLES
+#Définition des fichiers cleaned
 fichiers_a_pousser = {
     'silver/caracteristiques_cleaned.csv': 'fact_caracteristiques',
     'silver/usagers_cleaned.csv': 'dim_usagers',
@@ -43,70 +38,67 @@ fichiers_a_pousser = {
 }
 
 def executer_scripts_sql(engine, dossier_sql="Tables_SQL"):
-    """Lit et exécute tous les fichiers .sql du dossier scripts_sql."""
-    if not os.path.exists(dossier_sql):
-        print(f"⚠️ Dossier '{dossier_sql}' non trouvé. Pas de vues créées.")
-        return
 
+    """Lit et exécute tous les fichiers .sql du dossier tables_SQL."""
+    
+    if not os.path.exists(dossier_sql):
+        print(f"Dossier '{dossier_sql}' non trouvé. Pas de vues créées.")
+        return
     fichiers = sorted([f for f in os.listdir(dossier_sql) if f.endswith('.sql')])
     
     try:
         with engine.begin() as conn:
             for nom_fichier in fichiers:
                 chemin = os.path.join(dossier_sql, nom_fichier)
+
                 with open(chemin, 'r', encoding='utf-8') as f:
                     sql = f.read()
-                print(f"🔄 Exécution du script : {nom_fichier}...")
+
+                print(f"Exécution du script : {nom_fichier}...")
                 conn.execute(text(sql))
-            print("✅ Toutes les vues ont été recréées.")
+            print("Toutes les vues ont été recréées.")
     except Exception as e:
-        print(f"❌ Erreur lors de la création des vues : {e}")
+        print(f"Erreur lors de la création des vues : {e}")
 
 def run_pipeline():
-    print(f"🚀 Début de l'automatisation vers : {DB_HOST}")
+    print(f"Début de l'automatisation vers : {DB_HOST}")
     
     try:
         for s3_key, table_name in fichiers_a_pousser.items():
             print(f"\n--- Traitement de la table : {table_name} ---")
             try:
-                # 1. Récupération S3
                 response = s3_client.get_object(Bucket=BUCKET_NAME, Key=s3_key)
                 df = pd.read_csv(io.BytesIO(response['Body'].read()), sep=',', low_memory=False)
-                print(f"📥 Données S3 récupérées ({len(df)} lignes).")
+                print(f"Données S3 récupérées ({len(df)} lignes).")
                 
-                # 2. Tentative de TRUNCATE + APPEND
+                #Tentative de TRUNCATE + APPEND si le APPEND échoue (ex: nouvelles colonnes), on utilise CASCADE pour tout supp puis on recrée tous  
                 try:
                     with engine.begin() as conn:
                         conn.execute(text(f'TRUNCATE TABLE "{table_name}" CASCADE;'))
                     
                     df.to_sql(table_name, engine, if_exists='append', index=False, method='multi', chunksize=3000)
-                    print(f"✅ Succès : Mise à jour en mode APPEND.")
+                    print(f"Mise à jour en mode APPEND.")
 
-                except Exception as e_append:
-                    # 3. Si l'append échoue (ex: nouvelles colonnes), on utilise CASCADE
-                    print(f"⚠️ Changement de structure détecté. Passage au mode DROP CASCADE...")
-                    
+                except Exception as e_append:                  
                     with engine.begin() as conn:
-                        # Le CASCADE va supprimer view_caract, view_usager, etc. automatiquement
-                        print(f"🔥 Suppression forcée de la table {table_name} (et de ses vues)...")
+                        print(f"Suppression forcée de la table {table_name} (et de ses vues)...")
                         conn.execute(text(f'DROP TABLE IF EXISTS "{table_name}" CASCADE;'))
                     
-                    # 4. On recrée la table avec la nouvelle structure
+                    #On recrée la table avec la nouvelle structure
                     df.to_sql(table_name, engine, if_exists='replace', index=False, method='multi', chunksize=3000)
-                    print(f"✅ Succès : Table {table_name} recréée proprement.")
+                    print(f"Table {table_name} recréée proprement.")
 
             except Exception as e:
-                print(f"❌ Erreur sur {table_name} : {e}")
+                print(f"Erreur sur {table_name} : {e}")
                 continue 
 
-        # # --- PHASE FINALE : RECRÉATION AUTOMATIQUE DES VUES ---
-        print("\n--- 🏗️ Reconstruction des Vues SQL ---")
+        print("\n--- Reconstruction des Vues SQL ---")
         executer_scripts_sql(engine)
 
-        print(f"\n✨ Félicitations ! Pipeline terminé et Power BI est à jour.")
+        print(f"\n Pipeline terminé et Power BI est à jour.")
 
     except Exception as e:
-        print(f"❌ Erreur inattendue dans le pipeline : {e}")
+        print(f"Erreur inattendue dans le pipeline : {e}")
 
 if __name__ == "__main__":
     run_pipeline()
