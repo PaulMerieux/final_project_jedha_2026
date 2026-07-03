b8e4792fa8951d180cbcbe9eecde4c9c6ac3aca0
paul_m <paulomerieux@gmail.com>
2026-07-03 18:25:20 +0200

    ajout download to bronze

--- run_all_py.py
+++ run_all_py.py
@@ -1,11 +1,12 @@
 import main
+import asyncio
 import script_connexion_RDS
 import bronze_downloader
 
 if __name__ == "__main__":
     print("DÉMARRAGE DU PIPELINE COMPLET (END-TO-END)")
 
-    bronze_downloader.run()
+    asyncio.run(bronze_downloader.run())
     main.run()
     script_connexion_RDS.run()
 
